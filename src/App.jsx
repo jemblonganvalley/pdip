516be65f28c126eb088f66443dc7c03ece444d17
jemblonganvalley <jemblonganvalley@gmail.com>
2020-08-04 09:27:09 -0700

    agil

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import PartaiPage from './components/pages/partai/PartaiPage';
 import MultimediaPage from './components/pages/multimedia/MultimediaPage';
 import BkbbPage from './components/pages/bkbb/BkbbPage'
 import KetuaUmum from './components/pages/KetuaUmum/KetuaUmum';
-import Multimedia from './components/pages/Multimedia/Multimedia';
 
 function App() {
   return (
