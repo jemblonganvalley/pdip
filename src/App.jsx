f5230d0b19abf9554f4c74a2c5b30a72b056e92a
mohamad-ridwan <64695651+mohamad-ridwan@users.noreply.github.com>
2020-08-04 15:17:33 +0700

    ridwanupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch, NavLink} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch, NavLink } from 'react-router-dom'
 import MainNavbar from './components/navbar/MainNavbar';
 import HomePage from './components/pages/home/HomePage';
 import MainFooter from './components/footer/MainFooter';
 import PartaiPage from './components/pages/partai/PartaiPage';
+import KetuaUmum from './components/pages/KetuaUmum/KetuaUmum';
+import Multimedia from './components/pages/Multimedia/Multimedia';
 
 function App() {
   return (
     <Router>
-        <MainNavbar/>
+      <MainNavbar />
       <div className="App">
-        
-            <Switch>
-              <Route path="/partai">
-                <PartaiPage/>
-              </Route>
 
-              <Route path="/">
-                <HomePage/>
-              </Route>
-            </Switch>
+        <Switch>
+          <Route path="/multimedia">
+            <Multimedia />
+          </Route>
+
+          <Route path="/ketuaUmum">
+            <KetuaUmum />
+          </Route>
+
+          <Route path="/partai">
+            <PartaiPage />
+          </Route>
+
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
       </div>
-        <MainFooter/>
+      <MainFooter />
     </Router>
   );
 }
