6740ef6615f4aee4e1179d2f91dad6b7e7aebbba
jemblonganvalley <jemblonganvalley@gmail.com>
2020-08-12 11:08:11 -0700

    update cardInformasi

--- src/components/cardInformasi/CardInformasi.jsx
+++ src/components/cardInformasi/CardInformasi.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './CardInformasi.scss'
+import {Link} from 'react-router-dom'
 
-
-const CardInformasi = ({image, image2, image3, image4, text, text2, text3, text4})=> {
+const CardInformasi = ({image, image2, image3, image4, text, text2, text3, text4, to1, to2, to3, to4})=> {
     return (
         <div className="cardMedia">
-            <div className="siaranLangsung mediaCard">
+            <Link to={to1} className="siaranLangsung mediaCard">
                 <img src={image} alt="" width="35" className="imgCards" />
                 <small className="smallCard">{text}</small>
-            </div>
-            <div className="videoDokumentasi mediaCard">
+            </Link>
+            <Link to={to2} className="videoDokumentasi mediaCard">
                 <img src={image2} alt="" width="35" className="imgCards" />
                 <small className="smallCard">{text2}</small>
-            </div>
-            <div className="videoInteraktif mediaCard">
+            </Link>
+            <Link to={to3} className="videoInteraktif mediaCard">
                 <img src={image3} alt="" width="35" className="imgCards" />
                 <small className="smallCard">{text3}</small>
-            </div>
-            <div className="siaranPers mediaCard">
+            </Link>
+            <Link to={to4} className="siaranPers mediaCard">
                 <img src={image4} alt="" width="35" className="imgCards" />
                 <small className="smallCard">{text4}</small>
-            </div>
+            </Link>
         </div>
     )
 }
