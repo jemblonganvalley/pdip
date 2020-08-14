deb6bf5faf414540c9f2f23d7036b89ae8e57d93
mohamad-ridwan <64695651+mohamad-ridwan@users.noreply.github.com>
2020-08-15 02:31:37 +0700

    update carousel berita

--- src/components/carouselberitapage2/CarouselBeritaPage2.jsx
+++ src/components/carouselberitapage2/CarouselBeritaPage2.jsx
@@ -5,6 +5,48 @@ import './CarouselBeritaPage2.scss'
 const CarouselBeritaPage2 = () => {
     return (
         <>
+            {/* Carousel Page2 */}
+            {/* Row 1 */}
+            <div className="row1">
+                <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        {/* Box Indicator */}
+                        <div className="box-indicator">
+                            <p className="txt-no-slide-in">
+                                2
+                            </p>
+                            <p className="txt-no-slide-total">
+                                / 34
+                            </p>
+                        </div>
+                        {/* END Box Indicator */}
+
+                        <div className="carousel-item active img1">
+
+                        </div>
+                        <div className="carousel-item img2">
+
+                        </div>
+                        <div className="carousel-item img3">
+
+                        </div>
+                    </div>
+
+                    <div className="box-btn">
+                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true" />
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true" />
+                            <span className="sr-only">Next</span>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            {/* END Row 1 */}
+            {/* END Carousel Page2 */}
         </>
     )
 }
--- src/components/pages/berita/page2/Page2.jsx
+++ src/components/pages/berita/page2/Page2.jsx
@@ -9,6 +9,7 @@ import detailartikel6 from '../../../../img/detailartikel6.jpg'
 import '../page2/Page2.scss'
 import BreadCrumbs from '../../../breadcrumbs/BreadCrumbs'
 import Cards from '../../../cards/MainCards'
+import CarouselBeritaPage2 from '../../../carouselberitapage2/CarouselBeritaPage2'
 
 const Page2 = () => {
 
@@ -35,44 +36,7 @@ const Page2 = () => {
                     {/* Column1 */}
                     <div className="col1-container-1">
                         {/* Row1 */}
-                        <div className="row1">
-                            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                                <div className="carousel-inner">
-                                    {/* Box Indicator */}
-                                    <div className="box-indicator">
-                                        <p className="txt-no-slide-in">
-                                            2
-                                        </p>
-                                        <p className="txt-no-slide-total">
-                                            / 34
-                                        </p>
-                                    </div>
-                                    {/* END Box Indicator */}
-
-                                    <div className="carousel-item active img1">
-
-                                    </div>
-                                    <div className="carousel-item img2">
-
-                                    </div>
-                                    <div className="carousel-item img3">
-
-                                    </div>
-                                </div>
-
-                                <div className="box-btn">
-                                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                        <span className="carousel-control-prev-icon" aria-hidden="true" />
-                                        <span className="sr-only">Previous</span>
-                                    </a>
-                                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                                        <span className="carousel-control-next-icon" aria-hidden="true" />
-                                        <span className="sr-only">Next</span>
-                                    </a>
-
-                                </div>
-                            </div>
-                        </div>
+                        <CarouselBeritaPage2 />
                         {/* END Row1 */}
 
                         {/* Row2 */}
