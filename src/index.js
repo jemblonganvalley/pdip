4b88a19ac14fe62463a73d4dbbb842ddc7e45ba1
jemblonganvalley <jemblonganvalley@gmail.com>
2020-11-15 00:22:50 +0700

    registered pwa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {StoreProvider} from 'easy-peasy'
-import { globalState } from './database/globalState'
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { StoreProvider } from "easy-peasy";
+import { globalState } from "./database/globalState";
 
 ReactDOM.render(
   <StoreProvider store={globalState}>
@@ -14,10 +12,10 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </StoreProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
