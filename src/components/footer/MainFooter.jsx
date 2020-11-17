f20537379338b08adea201d299f47baee777dc39
jemblonganvalley <jemblonganvalley@gmail.com>
2020-11-17 15:01:37 +0700

    update footer

--- src/components/footer/MainFooter.jsx
+++ src/components/footer/MainFooter.jsx
@@ -1,77 +1,79 @@
-import React from 'react'
-import Logo from '../../img/pdiperjuangan02.svg'
-import './MainFooter.scss'
-import Subscribe from '../subscribe/Subscribe'
+import React from "react";
+import Logo from "../../img/pdiperjuangan02.svg";
+import "./MainFooter.scss";
+import Subscribe from "../subscribe/Subscribe";
 
-const MainFooter = ()=> {
-    return (
-        <div className="wrapperFooter">
-            <div className="wrapperContent">
-
-                    <img src={Logo} alt="" width="100" height="100" className="logo-mainFooter"
-                    style={{
-                        objectFit: 'fill'
-                    }}
-                />
-                <span  className='wrapperSituation'>
-                
-
-                <div className="textFooter">
-                    <small className="heads">situation room pdi perjuangan</small>
-                    <small className="body">Situs pdiperjuangan.id adalah situs resmi PDI Perjuangan. Situs ini dikelola oleh Pusat Analisa dan Pengendali Situasi (Situation Room) yang dikepalai oleh Prananda Prabowo.</small>
-                    <small className="foot">Untuk dukungan dan partisipasi, kirimkan naskah atau artikel beserta foto kegiatan anda ke email : redaksi@pdiperjuangan.id</small>
-                </div>
-                </span>
-                  <address className="contact " >
-
-                        <a className="emailFooter row"
-                            href="mailto:redaksi@pdiperjuangan.id"
-                            target="_blank"
-                        >
-                            <div className="col">
-                                <i className="fa fa-envelope-o"></i> 
-                                <small className='ml-2'>
-                                    <b>
-                                        redaksi@pdiperjuangan.id
-                                    </b>
-                                </small>
-                            </div>
-                        </a>
-
-                         <a className="emailFooter row"
-                            href="tel:0213909925"
-                            target="_blank"
-                         >
-                            <div className="col">
-                                <i className="fa fa-phone"></i> 
-                                <small className='ml-2'>
-                                    <b>
-                                        021-3909925
-                                    </b>
-                                </small>
-                            </div>
-                        </a>
-
-                        <a className="emailFooter row" 
-                            href="https://goo.gl/maps/2dD2qXuAUH4aS4hY7"
-                            target="_blank"
-                        >
-                            <div className="col">
-                                <i className="fa fa-map-marker"></i> 
-                                <small className='ml-2'>
-                                    <b>
-                                        Jl. Diponegoro No. 58 Menteng Jakarta Pusat 10310
-                                    </b>
-                                </small>
-                            </div>
-                        </a>
-
-                        
-                </address>
-                    <Subscribe />
+const MainFooter = () => {
+  return (
+    <div className="wrapperFooter">
+      <div className="wrapperContent">
+        <img
+          src={Logo}
+          alt=""
+          width="100"
+          height="100"
+          className="logo-mainFooter"
+          style={{
+            objectFit: "fill",
+          }}
+        />
+        <span className="wrapperSituation">
+          <div className="textFooter">
+            <small className="heads">situation room pdi perjuangan</small>
+            <small className="body">
+              pdiperjuangan.id merupakan situs (website) resmi Dewan Pimpinan
+              Pusat Partai Demokrasi Indonesia Perjuangan (DPP PDI Perjuangan).
+              Situs yang digagas oleh Muhammad Prananda Prabowo ini merupakan
+              induk dari media sosial resmi Partai, di antaranya Facebook,
+              Twitter, Instagram dan Youtube, dan merupakan satu kesatuan dengan
+              aplikasi Media Pintar Perjuangan.
+            </small>
+            <small className="foot">
+              Untuk dukungan dan partisipasi, kirimkan naskah atau artikel
+              beserta foto kegiatan anda ke email : redaksi@pdiperjuangan.id
+            </small>
+          </div>
+        </span>
+        <address className="contact ">
+          <a
+            className="emailFooter row"
+            href="mailto:redaksi@pdiperjuangan.id"
+            target="_blank"
+          >
+            <div className="col">
+              <i className="fa fa-envelope-o"></i>
+              <small className="ml-2">
+                <b>redaksi@pdiperjuangan.id</b>
+              </small>
             </div>
-        </div>
-    )
-}
+          </a>
 
-export default MainFooter
+          <a className="emailFooter row" href="tel:0213909925" target="_blank">
+            <div className="col">
+              <i className="fa fa-phone"></i>
+              <small className="ml-2">
+                <b>021-3909925</b>
+              </small>
+            </div>
+          </a>
+
+          <a
+            className="emailFooter row"
+            href="https://goo.gl/maps/2dD2qXuAUH4aS4hY7"
+            target="_blank"
+          >
+            <div className="col">
+              <i className="fa fa-map-marker"></i>
+              <small className="ml-2">
+                <b>Jl. Diponegoro No. 58 Menteng Jakarta Pusat 10310</b>
+              </small>
+            </div>
+          </a>
+        </address>
+        <Subscribe />
+      </div>
+    </div>
+  );
+};
+
+export default MainFooter;
